40eb1c1487f53251851b12d0b65f84e4588d89b1
Rodrigo França <rfrancacosta@gmail.com>
2021-12-04 15:00:26 -0300

    Tornando a aplicação Stateless (não guarda estado)
    Resolve o problema do cookies

--- tutorial/src/main/java/br/com/franca/tutorial/config/security/WebSecurityConfig.java
+++ tutorial/src/main/java/br/com/franca/tutorial/config/security/WebSecurityConfig.java
@@ -3,15 +3,20 @@ package br.com.franca.tutorial.config.security;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and()
+        http.httpBasic()
+            .and()
                 .authorizeRequests()
                 .antMatchers("/api/emitir").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+            .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
     }
 }
