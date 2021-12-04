ff286a1c88890c57caff5fe4d315a0efe36100c4
Rodrigo França <rfrancacosta@gmail.com>
2021-12-04 15:55:17 -0300

    Configurando autenticação de usuários em memória

--- tutorial/src/main/java/br/com/franca/tutorial/config/security/WebSecurityConfig.java
+++ tutorial/src/main/java/br/com/franca/tutorial/config/security/WebSecurityConfig.java
@@ -1,12 +1,35 @@
 package br.com.franca.tutorial.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    PasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        System.out.println("Senha: " + bCryptPasswordEncoder.encode("123"));
+
+        auth.inMemoryAuthentication()
+                .withUser("rfrancacosta@gmail.com")
+                    .password(bCryptPasswordEncoder.encode("123456"))
+                    .roles("admin")
+                .and()
+                .withUser("bpaulalima@gmail.com")
+                .password(bCryptPasswordEncoder.encode("123456789"))
+                    .roles("operator");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,4 +45,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     }
+
+
 }
