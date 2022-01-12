b45fd654e579c9909c404617b879becb748f8324
Rodrigo França <rfrancacosta@gmail.com>
2022-01-12 00:56:16 -0300

    organizando os diretorios

--- api-enriquecimento/src/main/java/br/com/franca/apienriquecimento/domain/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.franca.apienriquecimento.domain.repository;
-
-import br.com.franca.apienriquecimento.domain.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
