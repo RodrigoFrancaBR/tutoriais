229eb39bec77df257348281fa3c8550987c83e02
Rodrigo França <rfrancacosta@gmail.com>
2022-03-13 01:51:58 -0300

    Deixando os serviços no mesmo compose

--- enriquecimento/api-enriquecimento/src/main/resources/import.sql
+++ enriquecimento/api-enriquecimento/src/main/resources/import.sql
@@ -1,3 +1,5 @@
+lock tables tb_pessoa;
+
 insert into tb_pessoa (id, nome, cpf) values (1, 'Aluno_1', '12345678912');
 insert into tb_pessoa (id, nome, cpf) values (2, 'Aluno_2', '12345678913');
 insert into tb_pessoa (id, nome, cpf) values (3, 'Aluno_3', '12345678914');
